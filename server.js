94d83690e075cabb57f34796ca9678249f01b683
albert.y <albert.y@justdial.com>
2022-02-07 22:07:42 +0530

    first commit with jwt

---
+++ server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+const posts = [
+  {
+    username: "johanein",
+    title: "software engineer",
+  },
+  {
+    username: "Albert",
+    title: "software engineer",
+  },
+];
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter(({ username } = {}) => username === req?.user?.name));
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body || {};
+  const user = {
+    name: username,
+  };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken });
+});
+
+app.listen(3000);
